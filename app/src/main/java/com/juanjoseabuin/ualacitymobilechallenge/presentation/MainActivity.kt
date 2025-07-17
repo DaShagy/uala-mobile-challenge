719258d6d854e35c5b7c3eb03ce522dca52dff22
Juan Jose Abuin <juan.abuin@globant.com>
2025-07-17 20:50:59 -0300

    CMC-01: Base project structure. Added neccesary libraries. Added cities.json as raw res

---
+++ app/src/main/java/com/juanjoseabuin/ualacitymobilechallenge/presentation/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.juanjoseabuin.ualacitymobilechallenge.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.juanjoseabuin.ualacitymobilechallenge.presentation.theme.UalaCityMobileChallengeTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val viewModel = MainViewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UalaCityMobileChallengeTheme {
+                val cities = viewModel.cities.collectAsState()
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LazyColumn(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        items(
+                            count = cities.value.size,
+                            key = { cities.value[it].id }
+                        ) {
+                            Text(text = cities.value[it].name)
+                        }
+                    }
+                }
+            }
+
+            LaunchedEffect(Unit) {
+                viewModel.loadCities(this@MainActivity)
+            }
+        }
+    }
+}
