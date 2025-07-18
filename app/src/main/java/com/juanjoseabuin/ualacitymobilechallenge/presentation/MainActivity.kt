3b0127df52a574c0c320391f45c9cf53e627fbb9
Juan Jose Abuin <juan.abuin@globant.com>
2025-07-17 22:15:09 -0300

    CMC-02: CityRepository interface and impl

--- app/src/main/java/com/juanjoseabuin/ualacitymobilechallenge/presentation/MainActivity.kt
+++ app/src/main/java/com/juanjoseabuin/ualacitymobilechallenge/presentation/MainActivity.kt
@@ -4,25 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.juanjoseabuin.ualacitymobilechallenge.data.repository.CityRepositoryImpl
+import com.juanjoseabuin.ualacitymobilechallenge.domain.repository.CityRepository
+import com.juanjoseabuin.ualacitymobilechallenge.presentation.utils.ViewModelDelegate.viewModel
 import com.juanjoseabuin.ualacitymobilechallenge.presentation.theme.UalaCityMobileChallengeTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel = MainViewModel()
+    private lateinit var cityRepository: CityRepository
+
+    private val viewModel by viewModel<MainViewModel> {
+        MainViewModel(cityRepository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        cityRepository = CityRepositoryImpl(applicationContext)
+
         enableEdgeToEdge()
         setContent {
             UalaCityMobileChallengeTheme {
@@ -43,7 +50,7 @@ class MainActivity : ComponentActivity() {
             }
 
             LaunchedEffect(Unit) {
-                viewModel.loadCities(this@MainActivity)
+                viewModel.loadCities()
             }
         }
     }
--- app/src/main/java/com/juanjoseabuin/ualacitymobilechallenge/presentation/MainViewModel.kt
+++ app/src/main/java/com/juanjoseabuin/ualacitymobilechallenge/presentation/MainViewModel.kt
@@ -1,38 +1,27 @@
 package com.juanjoseabuin.ualacitymobilechallenge.presentation
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import com.juanjoseabuin.ualacitymobilechallenge.R
 import com.juanjoseabuin.ualacitymobilechallenge.domain.model.City
+import com.juanjoseabuin.ualacitymobilechallenge.domain.repository.CityRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import java.io.InputStreamReader
 
-class MainViewModel: ViewModel() {
+class MainViewModel(
+    private val cityRepository: CityRepository
+): ViewModel() {
 
     private val _cities: MutableStateFlow<List<City>> = MutableStateFlow(listOf())
     val cities = _cities.asStateFlow()
 
-    fun loadCities(context: Context) {
+    fun loadCities() {
         // CMC-01: TODO Move this to its corresponding data source
         viewModelScope.launch {
-            try {
-                val inputStream = context.resources.openRawResource(R.raw.cities)
-                val reader = InputStreamReader(inputStream)
-
-                val cityListType = object : TypeToken<List<City>>() {}.type
-                val gson = Gson()
-                val cityList: List<City> = gson.fromJson(reader, cityListType)
-
-                _cities.value = cityList
-
-                reader.close()
-            } catch (e: Exception) {
-                e.printStackTrace()
+            cityRepository.getCities().onSuccess {
+                _cities.value = it
+            }.onFailure {
+                _cities.value = listOf()
             }
         }
     }
