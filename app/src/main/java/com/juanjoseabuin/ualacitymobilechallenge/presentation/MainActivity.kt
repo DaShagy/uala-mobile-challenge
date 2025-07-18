a136ce0c625ca8ed0e7a0f479e5fe3a7028ec532
Juan Jose Abuin <juan.abuin@globant.com>
2025-07-18 09:19:31 -0300

    CMC-03: simple search cities by prefix implemented

--- app/src/main/java/com/juanjoseabuin/ualacitymobilechallenge/presentation/MainActivity.kt
+++ app/src/main/java/com/juanjoseabuin/ualacitymobilechallenge/presentation/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -34,16 +37,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             UalaCityMobileChallengeTheme {
                 val cities = viewModel.cities.collectAsState()
+                val searchValue = viewModel.searchValue.collectAsState()
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LazyColumn(
+                    Column(
                         modifier = Modifier.padding(innerPadding)
                     ) {
-                        items(
-                            count = cities.value.size,
-                            key = { cities.value[it].id }
-                        ) {
-                            Text(text = cities.value[it].name)
+                        TextField (
+                            modifier = Modifier.fillMaxWidth(),
+                            value = searchValue.value,
+                            onValueChange = { viewModel.searchCity(it) }
+                        )
+
+                        LazyColumn {
+                            items(
+                                count = cities.value.size,
+                                key = { cities.value[it].id }
+                            ) {
+                                Text(text = cities.value[it].name)
+                            }
                         }
                     }
                 }
--- app/src/main/java/com/juanjoseabuin/ualacitymobilechallenge/presentation/MainViewModel.kt
+++ app/src/main/java/com/juanjoseabuin/ualacitymobilechallenge/presentation/MainViewModel.kt
@@ -4,25 +4,54 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.juanjoseabuin.ualacitymobilechallenge.domain.model.City
 import com.juanjoseabuin.ualacitymobilechallenge.domain.repository.CityRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val cityRepository: CityRepository
-): ViewModel() {
+) : ViewModel() {
 
     private val _cities: MutableStateFlow<List<City>> = MutableStateFlow(listOf())
     val cities = _cities.asStateFlow()
 
-    fun loadCities() {
-        // CMC-01: TODO Move this to its corresponding data source
+    private val _searchValue = MutableStateFlow("")
+    val searchValue = _searchValue.asStateFlow()
+
+    init {
+        // Observe changes to searchValue with debounce
         viewModelScope.launch {
-            cityRepository.getCities().onSuccess {
-                _cities.value = it
-            }.onFailure {
-                _cities.value = listOf()
-            }
+            _searchValue
+                .debounce(300L)
+                .distinctUntilChanged()
+                .collectLatest { prefix ->
+                    searchCities(prefix)
+                }
+        }
+    }
+
+    fun loadCities() = viewModelScope.launch(Dispatchers.IO) {
+        cityRepository.getCities().onSuccess {
+            _cities.value = it
+        }.onFailure {
+            _cities.value = listOf()
+        }
+    }
+
+
+    fun searchCity(prefix: String) {
+        _searchValue.value = prefix
+    }
+
+    private fun searchCities(prefix: String) = viewModelScope.launch(Dispatchers.IO) {
+        cityRepository.searchCities(prefix).onSuccess {
+            _cities.value = it
+        }.onFailure {
+            _cities.value = listOf()
         }
     }
 }
