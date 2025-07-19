f8ac0329d5628cc34ec0b3c92851c8b13cb75223
Juan Jose Abuin <juan.abuin@globant.com>
2025-07-19 10:01:59 -0300

    CMC-10 Updating viewmodel ui state to handle on going favorite toggles. Created a CityUiItem model to represent city in presentation layer

---
+++ app/src/main/java/com/juanjoseabuin/ualacitymobilechallenge/presentation/model/CityUiItem.kt
@@ -0,0 +1,8 @@
+package com.juanjoseabuin.ualacitymobilechallenge.presentation.model
+
+import com.juanjoseabuin.ualacitymobilechallenge.domain.model.City
+
+data class CityUiItem(
+    val city: City,
+    val isToggling: Boolean = false // This is the UI-specific state
+)
