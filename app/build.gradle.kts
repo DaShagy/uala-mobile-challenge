41626e965a6e0ec2bf5abcc052120b74b03977d1
Juan Jose Abuin <juan.abuin@globant.com>
2025-07-19 11:38:54 -0300

    CMC-11 adding dependencies for pagination

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,10 @@ dependencies {
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
 
+    implementation(libs.androidx.paging.runtime.ktx)
+    implementation(libs.androidx.paging.compose)
+    implementation(libs.androidx.room.paging)
+
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.mockk)
